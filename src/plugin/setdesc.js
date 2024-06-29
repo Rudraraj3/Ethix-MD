125190e566b663fcf9764310ea12395759062bca
𝞓𝞜𝙅𝞓𝞜 <147349281+Rudraraj3@users.noreply.github.com>
2024-06-29 22:42:41 +0530

    Update setdesc.js

--- src/plugin/setdesc.js
+++ src/plugin/setdesc.js
@@ -10,16 +10,16 @@ const setDescription = async (m, gss) => {
 
     if (!validCommands.includes(cmd)) return;
 
-    if (!m.isGroup) return m.reply("*📛 THIS COMMAND CAN ONLY BE USED IN GROUPS*");
+    if (!m.isGroup) return m.reply("*THIS COMMAND CAN ONLY BE USED IN GROUPS*");
     const groupMetadata = await gss.groupMetadata(m.from);
     const participants = groupMetadata.participants;
     const botAdmin = participants.find(p => p.id === botNumber)?.admin;
     const senderAdmin = participants.find(p => p.id === m.sender)?.admin;
 
-    if (!botAdmin) return m.reply("*📛 BOT MUST BE AN ADMIN TO USE THIS COMMAND*");
-    if (!senderAdmin) return m.reply("*📛 YOU MUST BE AN ADMIN TO USE THIS COMMAND*");
+    if (!botAdmin) return m.reply("*BOT MUST BE AN ADMIN TO USE THIS COMMAND*");
+    if (!senderAdmin) return m.reply("*YOU MUST BE AN ADMIN TO USE THIS COMMAND*");
 
-    if (!text) return m.reply("*📛 PLEASE PROVIDE A DESCRIPTION TO SET*");
+    if (!text) return m.reply("*PLEASE PROVIDE A DESCRIPTION TO SET*");
 
     await gss.groupUpdateDescription(m.from, text);
     m.reply(`Group Description Has Been Set To: ${text}`);
