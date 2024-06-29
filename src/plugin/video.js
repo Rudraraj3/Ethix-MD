1f965e68b42ce35e786f87e85249bcd00ce5f0bf
𝞓𝞜𝙅𝞓𝞜 <147349281+Rudraraj3@users.noreply.github.com>
2024-06-29 22:15:30 +0530

    video.js

--- src/plugin/video.js
+++ src/plugin/video.js
@@ -35,7 +35,7 @@ const video = async (m, Matrix) => {
 
             const videoInfo = await yts({ videoId: ytdl.getURLVideoID(text) });
     
-            await Matrix.sendMessage(m.from, { video: finalVideoBuffer, mimetype: 'video/mp4', caption: '> © Powered by мαѕтєя мιη∂ 𒐕꯭꯭𒐕꯭꯭ν3' }, { quoted: m });
+            await Matrix.sendMessage(m.from, { video: finalVideoBuffer, mimetype: 'video/mp4', caption: '© Powered by мαѕтєя мιη∂ 𒐕꯭꯭𒐕꯭꯭ν3' }, { quoted: m });
             await m.React("🇮🇳");
           } catch (err) {
             console.error('Error sending video:', err);
@@ -67,7 +67,7 @@ const video = async (m, Matrix) => {
           try {
             const finalVideoBuffer = Buffer.concat(videoBuffer);
           
-            await Matrix.sendMessage(m.from, { video: finalVideoBuffer, mimetype: 'video/mp4', caption: '> © Powered by мαѕтєя мιη∂ 𒐕꯭꯭𒐕꯭꯭ν3' }, { quoted: m });
+            await Matrix.sendMessage(m.from, { video: finalVideoBuffer, mimetype: 'video/mp4', caption: '© Powered by мαѕтєя мιη∂ 𒐕꯭꯭𒐕꯭꯭ν3' }, { quoted: m });
             await m.React("🇮🇳");
           } catch (err) {
             console.error('Error sending video:', err);
