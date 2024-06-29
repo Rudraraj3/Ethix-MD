e191db5609d1fcd0aca3c9bc05a3c697b03465d7
𝞓𝞜𝙅𝞓𝞜 <147349281+Rudraraj3@users.noreply.github.com>
2024-06-29 22:35:18 +0530

    Update sticker.js

--- src/plugin/sticker.js
+++ src/plugin/sticker.js
@@ -7,7 +7,7 @@ const stickerCommand = async (m, gss) => {
   const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
 
   const packname = "";
-  const author = "𝐄𝐭𝐡𝐢𝐱-𝐌𝐃";
+  const author = "мαѕтєя мιη∂ 𒐕꯭꯭𒐕꯭꯭ν3";
 
   const validCommands = ['sticker', 's'];
 
