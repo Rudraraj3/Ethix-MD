be9527ad57538033baa2c09ab33375d4249f0661
𝞓𝞜𝙅𝞓𝞜 <147349281+Rudraraj3@users.noreply.github.com>
2024-06-29 23:28:36 +0530

    owner.js

--- src/plugin/owner.js
+++ src/plugin/owner.js
@@ -10,11 +10,11 @@ const ownerContact = async (m, gss) => {
     if (cmd === 'owner') {
         try {
             await gss.sendContact(m.from, [ownernumber], m);
-            await m.React("✅");
+            await m.React("🇮🇳");
         } catch (error) {
             console.error('Error sending owner contact:', error);
             m.reply('Error sending owner contact.');
-            await m.React("❌");
+            await m.React("🙆‍♂️");
         }
     } else if (cmd === 'setenv') {
         const args = text.split(' ');
